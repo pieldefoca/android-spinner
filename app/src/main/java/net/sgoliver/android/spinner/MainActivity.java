de86912b1ac8dc9929fa4b8d01ab5363ebcf2d89
David <betibaskonia@gmail.com>
2016-02-16 15:42:23 +0100

    Prueba git

--- app/src/main/java/net/sgoliver/android/spinner/MainActivity.java
+++ app/src/main/java/net/sgoliver/android/spinner/MainActivity.java
@@ -26,15 +26,12 @@ public class MainActivity extends ActionBarActivity {
                 new String[]{"Elem1","Elem2","Elem3","Elem4","Elem5"};
 
         //Alternativa 1: Array java
-        ArrayAdapter<String> adaptador =
-                new ArrayAdapter<String>(this,
-                        android.R.layout.simple_spinner_item, datos);
-
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, datos);
+        /*
         //Alternativa 2: Recurso XML de tipo string-array
-        //ArrayAdapter<CharSequence> adaptador =
-        //	    ArrayAdapter.createFromResource(this,
-        //	        R.array.valores_array, android.R.layout.simple_spinner_item);
-
+        //ArrayAdapter<CharSequence> adaptador = ArrayAdapter.createFromResource(this, R.array.valores_array, android.R.layout.simple_spinner_item);
+        // Un comentario para probar git
+        */
         adaptador.setDropDownViewResource(
                 android.R.layout.simple_spinner_dropdown_item);
 
